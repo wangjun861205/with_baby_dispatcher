bf09da44be3451fb11a4efd4be108f97dba0a67c
wangjun <444055828@qq.com>
2022-07-11 23:39:32 +0800

    test success

--- src/main.rs
+++ src/main.rs
@@ -5,67 +5,38 @@ use actix_web::{
     web::{get, Data},
 };
 use dotenv::dotenv;
-use kafka::client::{KafkaClient, ProduceMessage, RequiredAcks};
-use r2d2::{ManageConnection, Pool};
+use rdkafka::{
+    message::OwnedHeaders,
+    producer::{FutureProducer, FutureRecord},
+    ClientConfig,
+};
 use std::time::Duration;
-
-struct KafkaClientManager {
-    address: String,
-}
-
-impl KafkaClientManager {
-    fn new(address: String) -> Self {
-        Self { address }
-    }
-}
-
-impl ManageConnection for KafkaClientManager {
-    type Error = Error;
-    type Connection = KafkaClient;
-    fn connect(&self) -> Result<Self::Connection, Self::Error> {
-        let mut client = KafkaClient::new(vec![self.address.clone()]);
-        client
-            .load_metadata_all()
-            .map_err(|e| error::ErrorInternalServerError(e))?;
-        Ok(client)
-    }
-    fn has_broken(&self, conn: &mut Self::Connection) -> bool {
-        conn.load_metadata_all().map_or(false, |_| true)
-    }
-
-    fn is_valid(&self, conn: &mut Self::Connection) -> Result<(), Self::Error> {
-        conn.load_metadata_all()
-            .map_err(|e| error::ErrorInternalServerError(e))
-    }
-}
 
 #[main]
 async fn main() -> std::io::Result<()> {
     dotenv().unwrap();
     actix_web::HttpServer::new(move || {
-        let kafka_server = dotenv::var("KAFKA_SERVER").unwrap();
-        let mgr = KafkaClientManager::new(kafka_server);
-        let pool = Pool::new(mgr).unwrap();
-        actix_web::App::new()
-            .app_data(Data::new(pool))
-            .route("/nearby_locations", get().to(nearby_locations))
+        actix_web::App::new().route("/nearby_locations", get().to(nearby_locations))
     })
     .bind("localhost:8001")?
     .run()
     .await
 }
 
-async fn nearby_locations(client: Data<Pool<KafkaClientManager>>) -> Result<String, Error> {
-    let msgs = vec![ProduceMessage::new(
-        "nearby_locations",
-        0,
-        None,
-        Some(b"hello"),
-    )];
-    client
-        .get()
-        .map_err(|e| error::ErrorInternalServerError(e))?
-        .produce_messages(RequiredAcks::One, Duration::new(5, 0), msgs)
-        .map_err(|e| error::ErrorInternalServerError(e))?;
-    Ok("success".into())
+async fn nearby_locations() -> Result<String, Error> {
+    let producer: FutureProducer = ClientConfig::new()
+        .set("bootstrap.servers", "localhost:12092")
+        .set("message.timeout.ms", "5000")
+        .create()
+        .unwrap();
+    let status = producer
+        .send(
+            FutureRecord::to("nearby_locations")
+                .key("key")
+                .payload("hello"),
+            Duration::from_secs(0),
+        )
+        .await
+        .unwrap();
+    Ok(format!("partition: {} offset: {}", status.0, status.1))
 }
